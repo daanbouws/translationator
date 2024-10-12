d3433a298772bb4f811d614e8f53ca71816aa1e0
Daan Bouws <dbouws@cloudsuite.com>
2024-10-12 16:21:50 +0200

    added some folders

--- src/Translator/store.ts
+++ src/Translator/store.ts
@@ -2,14 +2,22 @@ import { register } from './inject.js';
 
 export const TRANSLATIONS = Symbol('TRANSLATIONS');
 
+type TranslationsSet = Record<string, string>
+
 @register(TRANSLATIONS)
 class TranslationStore {
   translations: Map<string, string> = new Map();
 
-  initializeWith(translations: Record<string, string>) {
+  initializeWith(translations: TranslationsSet) {
     for (const translation in translations) {
       const exists = this.translations.get(translation) != undefined;
       if (exists) continue;
+      this.translations.set(translation, translations[translation] || '');
+    }
+  }
+
+  setTranslations(translations: TranslationsSet) {
+    for (const translation in translations) {
       this.translations.set(translation, translations[translation] || '');
     }
   }
